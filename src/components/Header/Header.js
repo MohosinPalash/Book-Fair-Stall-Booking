7a13ffe6f022ebd821fdb93672cbd19fe74721ca
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-09-26 19:52:27 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import cover from '../../cover.jpg'
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header">
+
+            <h3>Ekushye Book Fair 2022</h3>
+            <p>Registration Portal<br />
+                Total Budget: 100 Million</p>
+        </div>
+    );
+};
+
+export default Header;
