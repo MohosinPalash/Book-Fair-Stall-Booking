93e240309c29bf13a74244aa612f2509f25b9198
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-09-26 23:28:09 +0600

    Final Commit

--- src/components/Publisher/Publisher.js
+++ src/components/Publisher/Publisher.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus, faFacebookSquare } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { Button } from 'react-bootstrap';
 import './Publisher.css'
 
